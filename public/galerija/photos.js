dae494bc4192a24e6096845a23d5a12efb6a5001
Bostjan Skufca <bostjan@skufca.si>
2013-06-10 15:39:22 +0000

    Initial commit

---
+++ public/galerija/photos.js
@@ -0,0 +1,123 @@
+WPGgallery = new Object();
+WPGgallery.name = "Solkan, marec 2009";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "2.1.2010"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "DSC_2862.jpg";
+gPhotos[0].ImageWidth = 450;
+gPhotos[0].ImageHeight = 301;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 50;
+gPhotos[0].meta = new Object();
+gPhotos[0].meta["description"] = "Zaèetek dneva v Solkanu";
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "DSC_2868.jpg";
+gPhotos[1].ImageWidth = 450;
+gPhotos[1].ImageHeight = 301;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 50;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "DSC_2870.jpg";
+gPhotos[2].ImageWidth = 450;
+gPhotos[2].ImageHeight = 301;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 50;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "DSC_2871.jpg";
+gPhotos[3].ImageWidth = 450;
+gPhotos[3].ImageHeight = 301;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 50;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "DSC_2872.jpg";
+gPhotos[4].ImageWidth = 450;
+gPhotos[4].ImageHeight = 301;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 50;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "DSC_2876.jpg";
+gPhotos[5].ImageWidth = 450;
+gPhotos[5].ImageHeight = 301;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 50;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "DSC_2880.jpg";
+gPhotos[6].ImageWidth = 450;
+gPhotos[6].ImageHeight = 301;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 50;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "DSC_2886.jpg";
+gPhotos[7].ImageWidth = 450;
+gPhotos[7].ImageHeight = 301;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 50;
+gPhotos[7].meta = new Object();
+gPhotos[7].meta["description"] = "borov prelec";
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "DSC_2889.jpg";
+gPhotos[8].ImageWidth = 450;
+gPhotos[8].ImageHeight = 301;
+gPhotos[8].ThumbWidth = 75;
+gPhotos[8].ThumbHeight = 50;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "DSC_2892.jpg";
+gPhotos[9].ImageWidth = 450;
+gPhotos[9].ImageHeight = 301;
+gPhotos[9].ThumbWidth = 75;
+gPhotos[9].ThumbHeight = 50;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "DSC_2900.jpg";
+gPhotos[10].ImageWidth = 450;
+gPhotos[10].ImageHeight = 301;
+gPhotos[10].ThumbWidth = 75;
+gPhotos[10].ThumbHeight = 50;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "DSC_2911.jpg";
+gPhotos[11].ImageWidth = 450;
+gPhotos[11].ImageHeight = 301;
+gPhotos[11].ThumbWidth = 75;
+gPhotos[11].ThumbHeight = 50;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "DSC_2913.jpg";
+gPhotos[12].ImageWidth = 450;
+gPhotos[12].ImageHeight = 301;
+gPhotos[12].ThumbWidth = 75;
+gPhotos[12].ThumbHeight = 50;
+gPhotos[12].meta = new Object();
+gPhotos[12].meta["description"] = "Kanal ob Soèi";
+
