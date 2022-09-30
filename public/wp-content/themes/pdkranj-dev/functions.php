6dcf44559a96a40ec8a4dc5230f87e92e2603d1a
Bostjan Skufca Jese <bostjan@skufca.si>
2022-09-30 17:22:07 +0000

    Revert "Add development theme, add graphql support"
    This reverts commit 61dcf7a001760e6e878eec72ec4118d77aa25d73.

--- public/wp-content/themes/pdkranj-dev/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'inc/navwalker.php';
-require_once 'inc/pagination.php';
-require_once 'inc/graphql.php';
-require_once 'inc/utils.php';
-require_once 'inc/setup.php';
--- public/wp-content/themes/pdkranj/functions.php
+++ public/wp-content/themes/pdkranj/functions.php
@@ -2,6 +2,5 @@
 
 require_once 'inc/navwalker.php';
 require_once 'inc/pagination.php';
-require_once 'inc/graphql.php';
 require_once 'inc/utils.php';
 require_once 'inc/setup.php';
